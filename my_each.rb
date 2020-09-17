b008a8fc5f4e6604f1552a6fa88d046e75d08964
Alex Darvin <amdarvin14@gmail.com>
2020-09-17 14:24:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 def my_each(collectoin)
   i = 0
-  while i < collection.length
-  yield(collection|i|)
-  i = i+ 1
-end
+    while i < collection.length
+    yield(collection|i|)
+    i = i+ 1
+  end
   
 collection
 end
